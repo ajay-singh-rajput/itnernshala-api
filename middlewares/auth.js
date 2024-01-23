4b7dc65463915bef46b64cbd544de66d560acc91
ajay-singh-rajput <ajay091.ar@gmail.com>
2024-01-23 16:24:24 +0530

    some error fix

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,18 +1,19 @@
 const jwt = require('jsonwebtoken');
 const ErrorHandler = require('../utils/ErrorHandler');
 const { catchAsyncError } = require('./catchAsyncError');
+const { head } = require('../routes/homeRoutes');
 
 exports.isAuthenticated = catchAsyncError(async(req, res, next)=>{
     const headerData = req.headers.authorization
+    console.log(headerData);
     const checkData = {
         id:headerData.split(' ')[0],
         token:headerData.split(' ')[1],
         userType:headerData.split(' ')[2],
     }
 const {token} = checkData;
-// console.log(checkData);
 
-if(!token){
+if(!token || (token === 'undefined')){
     return next(new ErrorHandler("Please login to access the resource", 401));
 }
 const {id} = jwt.verify(token, process.env.JWT_SECRET);
